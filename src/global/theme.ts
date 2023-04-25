70b112f8455c9ee8727dd55703663193eab1ffbe
Fellipe Neves <fellipe2.0e@gmail.com>
2023-04-24 22:14:38 -0300

    feat: created new theme and set new theme type

---
+++ src/global/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        french_gray:"#acadbc",
+        cool_gray:"#9b9ece",
+        slate_blue:"#6665dd",
+        palatinate_blue:"#473bf0",
+        black:"#000500",
+        success: "#84DD63",
+        failure: "#BB342F",
+    }
+}
